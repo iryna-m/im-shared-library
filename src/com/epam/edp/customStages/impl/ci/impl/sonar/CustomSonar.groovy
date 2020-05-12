849f3e13903e0821427f6bfc8542c615ae82dd1c
iryna-m <55385079+iryna-m@users.noreply.github.com>
2020-05-12 17:34:38 +0300

    Create CustomSonar.groovy

---
+++ src/com/epam/edp/customStages/impl/ci/impl/sonar/CustomSonar.groovy
@@ -0,0 +1,13 @@
+package com.epam.edp.customStages.impl.ci.impl.sonar
+
+import com.epam.edp.stages.impl.ci.ProjectType
+import com.epam.edp.stages.impl.ci.Stage
+
+@Stage(name = "sonar", buildTool = ["maven"], type = [ProjectType.APPLICATION, ProjectType.AUTOTESTS, ProjectType.LIBRARY])
+class CustomSonar {
+    Script script
+    void run(context) {
+        script.sh "echo 'Your custom logic of the stage'"
+    }
+}
+return CustomSonar
